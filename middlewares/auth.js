ca5fa1008144a5881616854f304eccb5bbbe8ab0
Anh <anhlq1209@gmail.com>
2021-04-25 11:18:03 +0700

    cm update page admin

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,14 +1,17 @@
 const asyncHandler = require('express-async-handler');
 const Admin = require('../models/admin');
+const Member = require('../models/member');
 
 module.exports = asyncHandler(async function(req, res, next) {
     const { adminId } = req.session;
     if (adminId) {
         const admin = await Admin.findById(adminId);
         if (admin) {
+            req.currentAdmin = admin;
             res.locals.currentAdmin = admin;
+            const members = await Member.findAll();
+            res.locals.currentListMember = members;
         }
     }
-    /* res.locals.regThanks = false; */
     next();
 });
